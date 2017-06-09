60b1dba81369b620c69d816b0e4cf53be26de6db
Tianxiao Jiang <jtxinnocence@gmail.com>
2017-06-09 15:59:33 -0500

    add home page

--- blog/static/blog/js/home.js
+++ blog/static/blog/js/home.js
@@ -1,16 +1,48 @@
+//Define a custom non-enumerable sortBy function using a Schwartzian transform on all arrays :
+//If your date is not directly comparable, make a comparable date out of it, e.g.
+//array.sortBy(function(o){ return new Date( o.date ) });
+
+(function(){
+  if (typeof Object.defineProperty === 'function'){
+    try{Object.defineProperty(Array.prototype,'sortBy',{value:sb}); }catch(e){}
+  }
+  if (!Array.prototype.sortBy) Array.prototype.sortBy = sb;
+
+  function sb(f){
+    for (var i=this.length;i;){
+      var o = this[--i];
+      this[i] = [].concat(f.call(o,o,i),o);
+    }
+    this.sort(function(a,b){
+      for (var i=0,len=a.length;i<len;++i){
+        if (a[i]!=b[i]) return a[i]<b[i]?-1:1;
+      }
+      return 0;
+    });
+    for (var i=this.length;i;){
+      this[--i]=this[i][this[i].length-1];
+    }
+    return this;
+  }
+})();
+// Sort by date, then score (reversed), then name
+array.sortBy(function(o){ return [ o.date, -o.score, o.name ] };
 $(function(){
 	$.getJSON("/blog/get/all/articles",function(data){
 		console.log(data.articles);
 		var content='';
-		for (var index=0; index<data.articles.length; ++index)
+		articles=data.articles;
+		articles.sortBy(function(o){ return new Date( -o.datetime ) });
+		for (var index=0; index<articles.length; ++index)
 		{
-			content+='<div class="comment-content">';
-			content+=data.articles[index].excerpt;
-			content+='</div></br>';
-			content+='<div class="comment-reader">';
-			content+='<p>';
-
-			var date=new Date(data.articles[index].datetime);
+			content+='<div class="excerpt-item">';
+			var date=new Date(articles[index].datetime);
+			content+='<a class="excerpt-title" href="/blog/';
+			content+=articles[index].file;
+			content+='">';
+			content+=articles[index].title;
+			content+='</a>';
+			content+='<p class="excerpt-time">';
 			content+=monthNames[date.getMonth()];
 			content+=" ";
 			content+=date.getDay();
@@ -20,10 +52,11 @@ $(function(){
 			content+=date.getHours();
 			content+=":";
 			content+=pad(date.getMinutes());
-
-			content+='</br>';
-			content+=data.articles[index].title;
-			content+='</p></div>';
+			content+='</p>';
+			content+='<p class="excerpt-content">';
+			content+=articles[index].excerpt;
+			content+='</p></br>';
+			content+='</div>'
 		}
 
 		$('#excerpts').html(content);
