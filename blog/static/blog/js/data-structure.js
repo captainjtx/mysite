e7cbee64f04793af7f90b34d04eb0413e451cb03
Tianxiao Jiang <jtxinnocence@gmail.com>
2017-06-10 23:26:30 -0500

    Use relative path in static file

--- blog/static/blog/js/data-structure.js
+++ blog/static/blog/js/data-structure.js
@@ -1,21 +1,21 @@
 $(function(){
-	$('#img-stack').attr('src',staticURL+"image/stack.png");
-	$('#img-queue').attr('src',staticURL+"image/queue.png");
-	$('#img-list').attr('src',staticURL+"image/list.png");
-	$('#img-radix').attr('src',staticURL+"image/radix-sort.png");
-	$('#img-bucket').attr('src',staticURL+"image/bucket-sort.png");
+	$('#img-stack').attr('src',"../image/stack.png");
+	$('#img-queue').attr('src',"../image/queue.png");
+	$('#img-list').attr('src',"../image/list.png");
+	$('#img-radix').attr('src',"../image/radix-sort.png");
+	$('#img-bucket').attr('src',"../image/bucket-sort.png");
 });
 $.when(
-		$.get(staticURL+'code/DataStructures/stack.cpp', function (result) {
+		$.get('../code/DataStructures/stack.cpp', function (result) {
 			$('#user-src-1').text(result);
 		}),
-		$.get(staticURL+'code/DataStructures/queue.cpp', function (result) {
+		$.get('../code/DataStructures/queue.cpp', function (result) {
 			$('#user-src-2').text(result);
 		}),
-		$.get(staticURL+'code/DataStructures/list.cpp', function (result) {
+		$.get('../code/DataStructures/list.cpp', function (result) {
 			$('#user-src-3').text(result);
 		}),
-		$.get(staticURL+'code/DataStructures/list-sentinel.cpp', function (result) {
+		$.get('../code/DataStructures/list-sentinel.cpp', function (result) {
 			$('#user-src-4').text(result);
 		})
 ).then(function(){
--- blog/static/blog/js/sort.js
+++ blog/static/blog/js/sort.js
@@ -1,31 +1,31 @@
 $(function(){
-	$('#img-insertion').attr('src',staticURL+"image/insertion-sort.png");
-	$('#img-partition').attr('src',staticURL+"image/partition.png");
-	$('#img-max-heap').attr('src',staticURL+"image/max-heap.png");
-	$('#img-build-max-heap').attr('src',staticURL+"image/build-max-heap.png");
-	$('#img-radix').attr('src',staticURL+"image/radix-sort.png");
-	$('#img-bucket').attr('src',staticURL+"image/bucket-sort.png");
+	$('#img-insertion').attr('src',"../image/insertion-sort.png");
+	$('#img-partition').attr('src',"../image/partition.png");
+	$('#img-max-heap').attr('src',"../image/max-heap.png");
+	$('#img-build-max-heap').attr('src',"../image/build-max-heap.png");
+	$('#img-radix').attr('src',"../image/radix-sort.png");
+	$('#img-bucket').attr('src',"../image/bucket-sort.png");
 });
 $.when(
-		$.get(staticURL+'code/SortingAlgorithms/insertSort.cpp', function (result) {
+		$.get('../code/SortingAlgorithms/insertSort.cpp', function (result) {
 			$('#user-src-1').text(result);
 		}),
-		$.get(staticURL+'code/SortingAlgorithms/mergeSort.cpp', function (result) {
+		$.get('../code/SortingAlgorithms/mergeSort.cpp', function (result) {
 			$('#user-src-2').text(result);
 		}),
-		$.get(staticURL+'code/SortingAlgorithms/quickSort.cpp', function (result) {
+		$.get('../code/SortingAlgorithms/quickSort.cpp', function (result) {
 			$('#user-src-3').text(result);
 		}),
-		$.get(staticURL+'code/SortingAlgorithms/heapSort.cpp', function (result) {
+		$.get('../code/SortingAlgorithms/heapSort.cpp', function (result) {
 			$('#user-src-4').text(result);
 		}),
-		$.get(staticURL+'code/SortingAlgorithms/countSort.cpp', function (result) {
+		$.get('../code/SortingAlgorithms/countSort.cpp', function (result) {
 			$('#user-src-5').text(result);
 		}),
-		$.get(staticURL+'code/SortingAlgorithms/radixSort.cpp', function (result) {
+		$.get('../code/SortingAlgorithms/radixSort.cpp', function (result) {
 			$('#user-src-6').text(result);
 		}),
-		$.get(staticURL+'code/SortingAlgorithms/bucketSort.cpp', function (result) {
+		$.get('../code/SortingAlgorithms/bucketSort.cpp', function (result) {
 			$('#user-src-7').text(result);
 		})
 ).then(function(){
