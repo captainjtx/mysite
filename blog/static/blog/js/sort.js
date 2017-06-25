4fa04d94cae3cdbc1c38561256a3441f093a02dc
Tianxiao Jiang <jtxinnocence@gmail.com>
2017-06-25 15:41:49 -0500

    use js relative url

--- blog/static/blog/js/sort.js
+++ blog/static/blog/js/sort.js
@@ -1,36 +1,38 @@
 $(function(){
-	$('#img-insertion').attr('src',"../image/insertion-sort.png");
-	$('#img-partition').attr('src',"../image/partition.png");
-	$('#img-max-heap').attr('src',"../image/max-heap.png");
-	$('#img-build-max-heap').attr('src',"../image/build-max-heap.png");
-	$('#img-radix').attr('src',"../image/radix-sort.png");
-	$('#img-bucket').attr('src',"../image/bucket-sort.png");
-});
-$.when(
-		$.get('../code/SortingAlgorithms/insertSort.cpp', function (result) {
-			$('#user-src-1').text(result);
-		}),
-		$.get('../code/SortingAlgorithms/mergeSort.cpp', function (result) {
-			$('#user-src-2').text(result);
-		}),
-		$.get('../code/SortingAlgorithms/quickSort.cpp', function (result) {
-			$('#user-src-3').text(result);
-		}),
-		$.get('../code/SortingAlgorithms/heapSort.cpp', function (result) {
-			$('#user-src-4').text(result);
-		}),
-		$.get('../code/SortingAlgorithms/countSort.cpp', function (result) {
-			$('#user-src-5').text(result);
-		}),
-		$.get('../code/SortingAlgorithms/radixSort.cpp', function (result) {
-			$('#user-src-6').text(result);
-		}),
-		$.get('../code/SortingAlgorithms/bucketSort.cpp', function (result) {
-			$('#user-src-7').text(result);
-		})
-).then(function(){
-	initAceEditor();
-	//$.getScript('/js/prism.js');
+	var scripts = document.getElementsByTagName("script"),src = scripts[scripts.length-1].src;
+	$('#img-insertion').attr('src',src+"/../../image/insertion-sort.png");
+	$('#img-partition').attr('src',src+"/../../image/partition.png");
+	$('#img-max-heap').attr('src',src+"/../../image/max-heap.png");
+	$('#img-build-max-heap').attr('src',src+"/../../image/build-max-heap.png");
+	$('#img-radix').attr('src',src+"/../../image/radix-sort.png");
+	$('#img-bucket').attr('src',src+"/../../image/bucket-sort.png");
+
+	$.when(
+			$.get(src+'/../../code/SortingAlgorithms/insertSort.cpp', function (result) {
+				$('#user-src-1').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/mergeSort.cpp', function (result) {
+				$('#user-src-2').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/quickSort.cpp', function (result) {
+				$('#user-src-3').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/heapSort.cpp', function (result) {
+				$('#user-src-4').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/countSort.cpp', function (result) {
+				$('#user-src-5').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/radixSort.cpp', function (result) {
+				$('#user-src-6').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/bucketSort.cpp', function (result) {
+				$('#user-src-7').text(result);
+			})
+	).then(function(){
+		initAceEditor();
+		//$.getScript('/js/prism.js');
+	});
 });
 
 
