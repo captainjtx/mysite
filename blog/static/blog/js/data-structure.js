eff708489db786595e8a35f1fba2eb6a2e7da6dc
Tianxiao Jiang <jtxinnocence@gmail.com>
2017-06-25 17:21:28 -0500

    use relative path in js

--- blog/static/blog/js/data-structure.js
+++ blog/static/blog/js/data-structure.js
@@ -1,24 +1,24 @@
 (function(){
-	var scripts = document.getElementsByTagName("script"),src = scripts[scripts.length-1].src;
+//	var scripts = document.getElementsByTagName("script"),src = scripts[scripts.length-1].src;
 	$(function(){
-		$('#img-stack').attr('src',src+"/../../image/stack.png");
-		$('#img-queue').attr('src',src+"/../../image/queue.png");
-		$('#img-list').attr('src',src+"/../../image/list.png");
-		$('#img-radix').attr('src',src+"/../../image/radix-sort.png");
-		$('#img-bucket').attr('src',src+"/../../image/bucket-sort.png");
+		$('#img-stack').attr('src',"../image/stack.png");
+		$('#img-queue').attr('src',"../image/queue.png");
+		$('#img-list').attr('src',"../image/list.png");
+		$('#img-radix').attr('src',"../image/radix-sort.png");
+		$('#img-bucket').attr('src',"../image/bucket-sort.png");
 
 	});
 	$.when(
-			$.get(src+'/../../code/DataStructures/stack.cpp', function (result) {
+			$.get('../code/DataStructures/stack.cpp', function (result) {
 				$('#user-src-1').text(result);
 			}),
-			$.get(src+'/../../code/DataStructures/queue.cpp', function (result) {
+			$.get('../code/DataStructures/queue.cpp', function (result) {
 				$('#user-src-2').text(result);
 			}),
-			$.get(src+'/../../code/DataStructures/list.cpp', function (result) {
+			$.get('../code/DataStructures/list.cpp', function (result) {
 				$('#user-src-3').text(result);
 			}),
-			$.get(src+'/../../code/DataStructures/list-sentinel.cpp', function (result) {
+			$.get('../code/DataStructures/list-sentinel.cpp', function (result) {
 				$('#user-src-4').text(result);
 			})
 	      ).then(function(){
