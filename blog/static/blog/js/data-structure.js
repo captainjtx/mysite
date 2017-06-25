b714d6d53f7e69eeae7c603e0f442083879fcf03
Tianxiao Jiang <jtxinnocence@gmail.com>
2017-06-25 16:03:33 -0500

    js

--- blog/static/blog/js/data-structure.js
+++ blog/static/blog/js/data-structure.js
@@ -1,27 +1,28 @@
-$(function(){
+(function(){
 	var scripts = document.getElementsByTagName("script"),src = scripts[scripts.length-1].src;
-	$('#img-stack').attr('src',src+"/../../image/stack.png");
-	$('#img-queue').attr('src',src+"/../../image/queue.png");
-	$('#img-list').attr('src',src+"/../../image/list.png");
-	$('#img-radix').attr('src',src+"/../../image/radix-sort.png");
-	$('#img-bucket').attr('src',src+"/../../image/bucket-sort.png");
+	$(function(){
+		$('#img-stack').attr('src',src+"/../../image/stack.png");
+		$('#img-queue').attr('src',src+"/../../image/queue.png");
+		$('#img-list').attr('src',src+"/../../image/list.png");
+		$('#img-radix').attr('src',src+"/../../image/radix-sort.png");
+		$('#img-bucket').attr('src',src+"/../../image/bucket-sort.png");
 
-});
+	});
+	$.when(
+			$.get(src+'/../../code/DataStructures/stack.cpp', function (result) {
+				$('#user-src-1').text(result);
+			}),
+			$.get(src+'/../../code/DataStructures/queue.cpp', function (result) {
+				$('#user-src-2').text(result);
+			}),
+			$.get(src+'/../../code/DataStructures/list.cpp', function (result) {
+				$('#user-src-3').text(result);
+			}),
+			$.get(src+'/../../code/DataStructures/list-sentinel.cpp', function (result) {
+				$('#user-src-4').text(result);
+			})
+	      ).then(function(){
+		initAceEditor();
+		});
+})();
 
-$.when(
-		var scripts = document.getElementsByTagName("script"),src = scripts[scripts.length-1].src;
-		$.get(src+'/../../code/DataStructures/stack.cpp', function (result) {
-			$('#user-src-1').text(result);
-		}),
-		$.get(src+'/../../code/DataStructures/queue.cpp', function (result) {
-			$('#user-src-2').text(result);
-		}),
-		$.get(src+'/../../code/DataStructures/list.cpp', function (result) {
-			$('#user-src-3').text(result);
-		}),
-		$.get(src+'/../../code/DataStructures/list-sentinel.cpp', function (result) {
-			$('#user-src-4').text(result);
-		})
-      ).then(function(){
-	initAceEditor();
-});
--- blog/static/blog/js/sort.js
+++ blog/static/blog/js/sort.js
@@ -1,39 +1,40 @@
-$(function(){
+(function(){
 	var scripts = document.getElementsByTagName("script"),src = scripts[scripts.length-1].src;
-	$('#img-insertion').attr('src',src+"/../../image/insertion-sort.png");
-	$('#img-partition').attr('src',src+"/../../image/partition.png");
-	$('#img-max-heap').attr('src',src+"/../../image/max-heap.png");
-	$('#img-build-max-heap').attr('src',src+"/../../image/build-max-heap.png");
-	$('#img-radix').attr('src',src+"/../../image/radix-sort.png");
-	$('#img-bucket').attr('src',src+"/../../image/bucket-sort.png");
+	$(function(){
+		$('#img-insertion').attr('src',src+"/../../image/insertion-sort.png");
+		$('#img-partition').attr('src',src+"/../../image/partition.png");
+		$('#img-max-heap').attr('src',src+"/../../image/max-heap.png");
+		$('#img-build-max-heap').attr('src',src+"/../../image/build-max-heap.png");
+		$('#img-radix').attr('src',src+"/../../image/radix-sort.png");
+		$('#img-bucket').attr('src',src+"/../../image/bucket-sort.png");
 
-});
+	});
 
+	$.when(
+			$.get(src+'/../../code/SortingAlgorithms/insertSort.cpp', function (result) {
+				$('#user-src-1').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/mergeSort.cpp', function (result) {
+				$('#user-src-2').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/quickSort.cpp', function (result) {
+				$('#user-src-3').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/heapSort.cpp', function (result) {
+				$('#user-src-4').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/countSort.cpp', function (result) {
+				$('#user-src-5').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/radixSort.cpp', function (result) {
+				$('#user-src-6').text(result);
+			}),
+			$.get(src+'/../../code/SortingAlgorithms/bucketSort.cpp', function (result) {
+				$('#user-src-7').text(result);
+			})
+		).then(function(){
+				initAceEditor();
+				//$.getScript('/js/prism.js');
+			});
 
-$.when(
-		var scripts = document.getElementsByTagName("script"),src = scripts[scripts.length-1].src;
-		$.get(src+'/../../code/SortingAlgorithms/insertSort.cpp', function (result) {
-			$('#user-src-1').text(result);
-		}),
-		$.get(src+'/../../code/SortingAlgorithms/mergeSort.cpp', function (result) {
-			$('#user-src-2').text(result);
-		}),
-		$.get(src+'/../../code/SortingAlgorithms/quickSort.cpp', function (result) {
-			$('#user-src-3').text(result);
-		}),
-		$.get(src+'/../../code/SortingAlgorithms/heapSort.cpp', function (result) {
-			$('#user-src-4').text(result);
-		}),
-		$.get(src+'/../../code/SortingAlgorithms/countSort.cpp', function (result) {
-			$('#user-src-5').text(result);
-		}),
-		$.get(src+'/../../code/SortingAlgorithms/radixSort.cpp', function (result) {
-			$('#user-src-6').text(result);
-		}),
-		$.get(src+'/../../code/SortingAlgorithms/bucketSort.cpp', function (result) {
-			$('#user-src-7').text(result);
-		})
-).then(function(){
-	initAceEditor();
-	//$.getScript('/js/prism.js');
-});
+})();
